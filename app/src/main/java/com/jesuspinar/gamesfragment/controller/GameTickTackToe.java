6bdfb6f2707b8e3e6ad53209ba665e3a3c3a449f
Jesus <jesuspl@tutanota.com>
2022-11-10 23:54:24 +0100

    Add new game method to ticktacktoe

--- app/src/main/java/com/jesuspinar/gamesfragment/controller/GameTickTackToe.java
+++ app/src/main/java/com/jesuspinar/gamesfragment/controller/GameTickTackToe.java
@@ -1,5 +1,7 @@
 package com.jesuspinar.gamesfragment.controller;
 
+import java.util.Arrays;
+
 public class GameTickTackToe {
 
     private final char[] board;
@@ -8,6 +10,10 @@ public class GameTickTackToe {
         board = new char[]{' ',' ',' ',
                              ' ',' ',' ',
                              ' ',' ',' '};
+    }
+    
+    public void newGame(){
+        Arrays.fill(board, ' ');
     }
 
     /**
@@ -19,33 +25,33 @@ public class GameTickTackToe {
     public void addMove(int row, int col, char xo){
         char empty = ' ';
         //ROW 1 ---------------------------
-        if (empty == board[0] && row  == 1 && col == 1){
+        if (empty == board[0] && row == 1 && col == 1){
             board[0] = xo;
         }
-        else if (empty == board[1] && row  == 1 && col == 2){
+        else if (empty == board[1] && row == 1 && col == 2){
             board[1] = xo;
         }
-        else if (empty == board[2] && row  == 1 && col == 3){
+        else if (empty == board[2] && row == 1 && col == 3){
             board[2] = xo;
         }
         //ROW 2 ---------------------------
-        else if (empty == board[3] && row  == 2 && col == 1){
+        else if (empty == board[3] && row == 2 && col == 1){
             board[3] = xo;
         }
-        else if (empty == board[4] && row  == 2 && col == 2){
+        else if (empty == board[4] && row == 2 && col == 2){
             board[4] = xo;
         }
-        else if (empty == board[5] && row  == 2 && col == 3){
+        else if (empty == board[5] && row == 2 && col == 3){
             board[5] = xo;
         }
         //ROW 3 ---------------------------
-        else if (empty == board[6] && row  == 3 && col == 1){
+        else if (empty == board[6] && row == 3 && col == 1){
             board[6] = xo;
         }
-        else if (empty == board[7] && row  == 3 && col == 2){
+        else if (empty == board[7] && row == 3 && col == 2){
             board[7] = xo;
         }
-        else if (empty == board[8] && row  == 3 && col == 3){
+        else if (empty == board[8] && row == 3 && col == 3){
             board[8] = xo;
         }
     }
